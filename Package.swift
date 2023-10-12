fea5870bf60ea9dbceb7cfa4c14c6750d5565d58
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 17:05:51 +0300

    1.8.0-8

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-7"
+let bnbPackageVersion: Version = "1.8.0-8"
 
 let package = Package(
     name: "BNBSkin",
@@ -12,7 +12,7 @@ let package = Package(
         .library(
             name: "BNBSkin",
             targets: [
-                "BNBSkinBNBSkinTarget",
+                "BNBSkin",
                 "BNBSkinBNBSdkCoreTarget",
                 "BNBSkinBNBEffectPlayerTarget",
                 "BNBSkinBNBScriptingTarget"
