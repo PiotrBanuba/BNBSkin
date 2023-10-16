6faa76200b9a83ada7b8959e73f5beb8b524c969
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-16 12:18:39 +0300

    1.8.0-17

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-16"
+let bnbPackageVersion: Version = "1.8.0-17"
 
 let package = Package(
     name: "BNBSkin",
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "BNBSkin",
+            type: .dynamic,
             targets: [
                 "BNBSkin",
                 "BNBSkin_BNBSdkCore",
